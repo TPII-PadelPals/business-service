2b7a3a44d796d65595018903963cfa740d5b4e74
amaruDuran <amaruduran@gmail.com>
2024-12-01 20:23:54 -0300

    rename: title fastapi padel courts

--- app/api/main.py
+++ app/api/main.py
@@ -6,7 +6,7 @@ api_router = APIRouter()
 api_router.include_router(items.router, prefix="/items", tags=["items"])
 api_router.include_router(businesses.router, prefix="/businesses", tags=["businesses"])
 api_router.include_router(
-    padel_courts.router, prefix="/padel-courts", tags=["padel_courts"]
+    padel_courts.router, prefix="/padel-courts", tags=["padel-courts"]
 )
 api_router.include_router(
     items_service.router, prefix="/items-service", tags=["items-service"]
