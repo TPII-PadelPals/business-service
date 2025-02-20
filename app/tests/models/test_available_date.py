4e85c61470567c76b6b56e3bf01e46cd872f22d7
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2025-02-20 19:29:38 -0300

    test: add test

--- app/tests/models/test_available_date.py
+++ app/tests/models/test_available_date.py
@@ -5,7 +5,7 @@ from datetime import date
 import pytest
 from sqlmodel.ext.asyncio.session import AsyncSession
 
-from app.models.available_date import AvailableDateCreate, AvailableDate
+from app.models.available_date import AvailableDateCreate, AvailableDate, AvailableDatePublic, AvailableDatesPublic
 from app.utilities.exceptions import NotAcceptableException
 
 
@@ -18,6 +18,7 @@ async def test_one_available_date_form_create():
         "number_of_games":1,
     }
     create = AvailableDateCreate(**data)
+    create.validate()
     # test
     available_date_list: list[AvailableDate] = AvailableDate.from_create(create)
     # assert
@@ -25,7 +26,7 @@ async def test_one_available_date_form_create():
     available_date = available_date_list[0]
     for key, value in data.items():
         assert getattr(available_date, key) == value
-    assert available_date.get_is_reserved() is False
+    assert available_date.is_reserved is False
 
 
 async def test_more_available_date_form_create():
@@ -38,6 +39,7 @@ async def test_more_available_date_form_create():
     }
     initial_hour_list = [5, 6, 7, 8, 9]
     create = AvailableDateCreate(**data)
+    create.validate()
     # test
     available_date_list: list[AvailableDate] = AvailableDate.from_create(create)
     # assert
@@ -50,7 +52,7 @@ async def test_more_available_date_form_create():
                 initial_hour_list.remove(data)
                 continue
             assert getattr(available_date, key) == value
-        assert available_date.get_is_reserved() is False
+        assert available_date.is_reserved is False
 
 
 async def test_limit_available_date_form_create():
@@ -63,6 +65,7 @@ async def test_limit_available_date_form_create():
     }
     initial_hour_list = [False] * 24
     create = AvailableDateCreate(**data)
+    create.validate()
     # test
     available_date_list: list[AvailableDate] = AvailableDate.from_create(create)
     # assert
@@ -74,7 +77,7 @@ async def test_limit_available_date_form_create():
                 initial_hour_list[pos] = True
                 continue
             assert getattr(available_date, key) == value
-        assert available_date.get_is_reserved() is False
+        assert available_date.is_reserved is False
     for initial_hour in initial_hour_list:
         assert initial_hour
 
@@ -111,3 +114,60 @@ async def test_create_invalid_number_games():
     assert e.value.detail == "The information is not acceptable. Reason: number_of_games cannot be less than 0."
 
 
+async def test_available_date_set_reserve():
+    data = {
+        "id": 15,
+        "court_id": uuid.uuid4(),
+        "business_id": uuid.uuid4(),
+        "date": date(2025, 1, 1),
+        "initial_hour": 23,
+        "number_of_games":1,
+        "is_reserved": False
+    }
+    available_date = AvailableDate(**data)
+    # test
+    available_date.set_reserve()
+    # assert
+    assert available_date.is_reserved is True
+
+
+async def test_public_from_private():
+    data = {
+        "id": 15,
+        "court_id": uuid.uuid4(),
+        "business_id": uuid.uuid4(),
+        "date": date(2025, 1, 1),
+        "initial_hour": 23,
+        "number_of_games":1,
+        "is_reserved": False
+    }
+    available_date = AvailableDate(**data)
+    # test
+    available_date_public = AvailableDatePublic.from_private(available_date)
+    # assert
+    for key, value in data.items():
+        if key == "id":
+            continue
+        assert getattr(available_date_public, key) == value
+
+
+async def test_publics_from_private():
+    data = {
+        "id": 15,
+        "court_id": uuid.uuid4(),
+        "business_id": uuid.uuid4(),
+        "date": date(2025, 1, 1),
+        "initial_hour": 23,
+        "number_of_games":1,
+        "is_reserved": False
+    }
+    available_date = AvailableDate(**data)
+    # test
+    available_dates_public = AvailableDatesPublic.from_private([available_date])
+    # assert
+    assert available_dates_public.count == 1
+    available_date_public = available_dates_public.data[0]
+    for key, value in data.items():
+        if key == "id":
+            continue
+        assert getattr(available_date_public, key) == value
