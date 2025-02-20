4d4d97785cbcfe6e207a996ccdd3d67f66ced3a1
Belletti Gabriel Ignacio <44848915+GIBelletti@users.noreply.github.com>
2025-02-20 19:29:21 -0300

    test: add tests and fix delete and set deletes in test DB

--- app/models/available_date.py
+++ app/models/available_date.py
@@ -17,8 +17,8 @@ class AvailableDateBase(SQLModel):
     TIME_LIMIT_MAX:ClassVar[int] = 23
     TIME_OF_GAME:ClassVar[int] = 1
 
-    court_id: uuid.UUID = Field(foreign_key="padel_courts.id")
-    business_id: uuid.UUID = Field(foreign_key="businesses.id")
+    court_id: uuid.UUID = Field(foreign_key="padel_courts.id", ondelete="CASCADE")
+    business_id: uuid.UUID = Field(foreign_key="businesses.id", ondelete="CASCADE")
     date: date = Field()
     initial_hour: int = Field(default=0, ge=TIME_LIMIT_MIN, le=TIME_LIMIT_MAX)
 
@@ -28,14 +28,14 @@ class AvailableDateCreate(AvailableDateBase):
     number_of_games: int = Field(default=1)
 
 
-    def get_number_of_games(self):
+    def __get_number_of_games(self):
         return self.number_of_games
 
 
     def validate(self) -> None:
         if self.number_of_games <= 0:
             raise NotAcceptableException("number_of_games cannot be less than 0")
-        if self.number_of_games * self.TIME_OF_GAME + self.initial_hour > self.TIME_LIMIT_MAX + 1:
+        if (self.number_of_games * self.TIME_OF_GAME) + self.initial_hour > self.TIME_LIMIT_MAX + 1:
             raise NotAcceptableException("number_of_games cannot exceed the time of one day")
 
 
@@ -44,22 +44,12 @@ class AvailableDateImmutable(SQLModel):
     id: BIGINT = Field(primary_key=True)
 
 
-# Private and mutable properties
-class AvailableDatePrivate(SQLModel):
+# Database model, database table inferred from class name
+class AvailableDate(AvailableDateBase, AvailableDateImmutable, table=True):
+    __tablename__ = AVAILABILITY_TABLE_NAME
     is_reserved: bool = Field(default=False)
 
 
-    def set_reserve(self):
-        self.is_reserved = True
-
-
-    def get_is_reserved(self):
-        return self.is_reserved
-
-
-# Database model, database table inferred from class name
-class AvailableDate(AvailableDateBase, AvailableDateImmutable, AvailableDatePrivate, table=True):
-    __tablename__ = AVAILABILITY_TABLE_NAME
     __table_args__ = (
         UniqueConstraint(
             "court_id",
@@ -78,7 +68,7 @@ class AvailableDate(AvailableDateBase, AvailableDateImmutable, AvailableDatePriv
     @classmethod
     def from_create(cls, create: AvailableDateCreate) -> list["AvailableDate"]:
         result = []
-        number_of_games = create.get_number_of_games()
+        number_of_games = create.__get_number_of_games()
         data = create.model_dump(exclude={'number_of_games'})
         # data["is_reserved"] = False
         for i_game in range(number_of_games):
@@ -89,8 +79,19 @@ class AvailableDate(AvailableDateBase, AvailableDateImmutable, AvailableDatePriv
         return result
 
 
+    def set_reserve(self):
+        self.is_reserved = True
+
+
+    # def get_is_reserved(self):
+    #     return self.is_reserved
+
+
 # Properties to return via API, id is always required
-class AvailableDatePublic(AvailableDateBase, AvailableDatePrivate):
+class AvailableDatePublic(AvailableDateBase):
+    is_reserved: bool = Field(default=False)
+
+
     @classmethod
     def from_private(cls, available_day: AvailableDate) -> "AvailableDatePublic":
         data = available_day.model_dump()
